22aa7e47ce42191873ad1afe295628b6253371ca
chenxu <chenxu2048@gmail.com>
2018-04-15 19:25:24 +0800

    修改代码

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     animals.append(Crow("Brown"))
     animals.append(Cat("John"))
     animals.append(Dog("Alice"))
+    animals.append(Crow("HAHA"))
+    animals.append(Cat("PAPA"))
+    animals.append(Dog("LALA"))
     for a in animals:
         a.speek()
 
